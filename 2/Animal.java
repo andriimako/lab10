d648dc1ec2c07f390ea726f079b0a2a45967e4f7
s17386 <61780276+s17386@users.noreply.github.com>
2020-06-04 13:39:39 +0200

    Add files via upload

---
+++ 2/Animal.java
@@ -0,0 +1,17 @@
+abstract class Animal {
+    String name;
+    String type;
+
+    public Animal (String name, String type) {
+        this.name = name;
+        this.type = type;
+
+    }
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
---
+++ 2/Cat.java
@@ -0,0 +1,24 @@
+public class Cat extends Animal {
+
+    public Cat(String name, String type) {
+        super(name, type);
+    }
+
+    @Override
+
+    public String getName(){
+        return name;
+    }
+
+    public String getType() {
+        return "Cat";
+    }
+
+    public void start(){
+        System.out.println("Cat starts!");
+    }
+    public void stop(){
+        System.out.println("Cat stopped!");
+    }
+
+}
